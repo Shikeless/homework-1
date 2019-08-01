8366817ef7d16bb8257aac9b985ec1d9efd26a01
Shikeless <Shikeless@gmail.com>
2019-08-01 16:19:03 +0300

    complete

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const app = () => (
+    <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+    </div>
+);
+
+export default app;
