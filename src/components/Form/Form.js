c8cd9fd3d83d2f222389258a512e7372ea739367
Shikeless <Shikeless@gmail.com>
2019-08-05 19:02:27 +0300

    fixing checkError method, adding validData

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,7 @@ import bond from './assets/bond_approve.jpg';
 
 const Error = ({error}) => <h1>{error}</h1>
 const Image = () => <img src={bond} alt="Bond"/>
+const validData = { firstName: 'James', lastName: 'Bond', password: '007' }
 
 class Form extends React.Component {
     state = { firstName: "", lastName: "", password: "", errorCode: "" };
@@ -10,12 +11,9 @@ class Form extends React.Component {
     setDefaultState = () => this.setState({ firstName: "", lastName: "", password: ""});
 
     checkError = () => {
-        const { firstName, lastName, password, errorCode } = this.state
-        if (errorCode !== "" && errorCode !== "Allowed" && firstName === "" && lastName === "" && password === "") {
-            return <Error error={this.state.errorCode}/>
-        } else if (errorCode === "Allowed") {
-            return <Image />
-        }
+        const { errorCode } = this.state;
+        if (errorCode === '') return null;
+        return errorCode === "Allowed" ? <Image /> : <Error error={this.state.errorCode}/>
     }
 
     handleSubmit = event => {
@@ -29,7 +27,7 @@ class Form extends React.Component {
                     this.setDefaultState
                 );
                 break;
-            } else if ((name === 'password' &&  value !== '007') || (name === 'lastName' &&  value !== 'Bond') || (name === 'firstName' &&  value !== 'James')) {
+            } else if (validData[name] !== value) {
                 this.setState({ 
                     errorCode: `wrong ${name}`
                 },
@@ -87,7 +85,7 @@ class Form extends React.Component {
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
-                {this.checkError.call()}
+                {this.checkError()}
             </div> 
         );
     }
