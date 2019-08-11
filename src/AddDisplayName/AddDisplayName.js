2088ec2c83d1bc54baa9e1b845e5d57133a82dbd
Shikeless <Shikeless@gmail.com>
2019-08-11 15:12:33 +0300

    to review

---
+++ src/AddDisplayName/AddDisplayName.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+/*
+  Напишите простой HOC и укажите для него displayName
+*/
+
+export const withDisplayName = (WrappedComponent) => {
+    class Component extends React.Component {
+      render () {
+        return <WrappedComponent />
+      }
+    }
+   
+    if (Component.displayName !== null) { 
+      Component.displayName = `HOC${getDisplayName(WrappedComponent)}`; 
+    } else {
+      Component.displayName = 'HOCComponent'
+    } 
+
+    function getDisplayName(WrappedComponent) {
+      return WrappedComponent.displayName || WrappedComponent.name || WrappedComponent.displayName ;
+    }
+   
+    return Component
+}
